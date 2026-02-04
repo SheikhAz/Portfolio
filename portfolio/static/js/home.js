a2585efb1141235df00ef1e56672335784f0a9c9
Shiekh Azmatulla <shiekazmatulla@gmail.com>
2026-02-05 03:20:41 +0530

    added

--- portfolio/static/js/home.js
+++ portfolio/static/js/home.js
@@ -1,5 +1,6 @@
 const abstractImg = document.querySelector(".abstract-img");
 window.addEventListener("scroll", () => {
   const scrollY = window.scrollY;
-  abstractImg.style.transform = `translate(${scrollY * 0.25}px, ${scrollY * -0.05}px)`;
+  abstractImg.style.transform = `translate(${scrollY * 0.15}px, ${scrollY * -0.05}px)`;
 });
+
